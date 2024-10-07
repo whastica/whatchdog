5360681f7dd304c18f22367c6f23a20a652737df
whastica <drlook92@gmail.com>
2024-10-07 09:54:56 -0500

    Creacion arquitectura microservicio reporte

---
+++ watchdogReport/src/test/java/com/devInnovators/watchdogReport/WatchdogReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devInnovators.watchdogReport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchdogReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
