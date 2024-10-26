38918cfc9056f5f61757daca083d9e274159b5dd
whastica <drlook92@gmail.com>
2024-10-26 08:54:30 -0500

    Creacion microservicio revision priorizacion

---
+++ WhatchdogRevisionPriorizacion/src/main/java/com/devInnovators/WhatchdogRevisionPriorizacion/WhatchdogRevisionPriorizacionApplication.java
@@ -0,0 +1,13 @@
+package com.devInnovators.WhatchdogRevisionPriorizacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatchdogRevisionPriorizacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatchdogRevisionPriorizacionApplication.class, args);
+	}
+
+}
