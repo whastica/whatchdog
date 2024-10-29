fa2e6fcf3a7ef951c5d0ac6a1f32c420babcced6
whastica <drlook92@gmail.com>
2024-10-29 10:45:57 -0500

    Creacion del microservicio notificacion

---
+++ WatchdogNotificacion/src/main/java/com/DevInnovators/WatchdogNotificacion/WatchdogNotificacionApplication.java
@@ -0,0 +1,13 @@
+package com.DevInnovators.WatchdogNotificacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchdogNotificacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchdogNotificacionApplication.class, args);
+	}
+
+}
---
+++ WatchdogNotificacion/src/test/java/com/DevInnovators/WatchdogNotificacion/WatchdogNotificacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevInnovators.WatchdogNotificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchdogNotificacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
