859f6016ee46aed4c40e377fde96db36dee8b175
whastica <drlook92@gmail.com>
2024-10-30 07:17:42 -0500

    intentado arreglar la sync

--- Whatchdog/src/main/java/com/devInnovators/Whatchdog/Query/domain/model/AdminC.java
+++ Whatchdog/src/main/java/com/devInnovators/Whatchdog/Query/domain/model/AdminC.java
@@ -1,15 +1,14 @@
 package com.devInnovators.Whatchdog.Query.domain.model;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 
 @Data
--- Whatchdog/src/main/java/com/devInnovators/Whatchdog/Query/domain/repository/QueryReportRepository.java
+++ Whatchdog/src/main/java/com/devInnovators/Whatchdog/Query/domain/repository/QueryReportRepository.java
@@ -1,22 +1,18 @@
 package com.devInnovators.Whatchdog.Query.domain.repository;
 
-import com.devInnovators.Whatchdog.Query.domain.model.Report;
-
-
-import com.devInnovators.Whatchdog.Query.domain.model.Status;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-
-
-import java.util.List;
+import com.devInnovators.Whatchdog.Query.domain.model.Report;
+import com.devInnovators.Whatchdog.Query.domain.model.Status;
 
 @Repository
 public interface QueryReportRepository extends MongoRepository<Report, String> {
     List<Report> findByStatus(Status status); 
     List<Report> findByCategoryIssue(String categoryIssue);
-    @Query("{ 'idAdminC.id': ?0 }")
+    @Query("{'AdminC.id': ?0 }")
     List<Report> findByIdAdminC(String idAdminC);
 }
